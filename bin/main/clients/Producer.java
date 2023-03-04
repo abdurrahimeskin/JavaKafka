7779576c066d55cb9811aea2ff5ca831e40d99d1
Abdurrahim Eskin <abdurrahimeskincmpe@gmail.com>
2023-03-04 02:01:48 +0100

    Test classes and Producer Class added.

---
+++ bin/main/clients/Producer.java
@@ -0,0 +1,19 @@
+public class clients.Producer {
+static final java.lang.String DRIVER_FILE_PREFIX;
+
+static final java.lang.String KAFKA_TOPIC;
+
+public clients.Producer();
+        Code:
+        0: aload_0
+        1: invokespecial                 // Method java/lang/Object."<init>":()V
+        4: return
+
+public static void main(java.lang.String[]) throws java.io.IOException, java.lang.InterruptedException;
+        Code:
+        0: new                          // class java/lang/Error
+        3: dup
+        4: ldc                         // String Unresolved compilation problems: \n\tSyntax error on tokens, SimpleName expected instead\n\tSyntax error, insert \": Expression\" to complete Expression\n\tSyntax error, insert \")\" to complete Expression\n\tSyntax error on tokens, delete these tokens\n\tSyntax error on tokens, ArgumentList expected instead\n\tSyntax error on tokens, delete these tokens\n\tSyntax error, insert \")\" to complete Expression\n\tSyntax error, insert \";\" to complete Statement\n\tSyntax error, insert \"}\" to complete Block\n
+        6: invokespecial                  // Method java/lang/Error."<init>":(Ljava/lang/String;)V
+        9: athrow
+        }
